39e8e5340a2725730d2bdf589f64fb96d7c44bda
Oleg Hluhk <glug.oleg@gmail.com>
2022-04-12 20:03:07 +0300

    test api

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,29 @@
+import { useEffect } from 'react';
+import md5 from 'js-md5';
+
 const Home: React.FC = () => {
+  useEffect(() => {
+    const ts = Number(new Date());
+
+    const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY || '';
+    const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY || '';
+
+    const hash = md5.create();
+    hash.update(ts + PRIVATE_KEY + PUBLIC_KEY);
+
+    fetch(
+      `http://gateway.marvel.com/v1/public/characters?apikey=${PUBLIC_KEY}&ts=${ts}&hash=${hash.toString()}`
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          res.json().then((data) => {
+            console.log(data);
+          });
+        }
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return <>Home</>;
 };
 
